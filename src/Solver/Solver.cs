d746931f80040fe1f9e5e47d5d8f604487512f4f
Tobias Rink <tobias.rink@rwth-aachen.de>
2022-02-16 14:30:10 +0100

    Solver counts valid equations

--- src/Solver/Solver.cs
+++ src/Solver/Solver.cs
@@ -2,24 +2,76 @@ using System.Linq;
 using System.Collections.Generic;
 using System;
 using static Solver.EquationComponent;
+using System.Threading.Tasks;
 
 namespace Solver
 {
     public static class Solver
     {
-        public static Equation Solve(Dictionary<Equation, EquationComparison> info)
+        public static Equation Guess(List<EquationComparison> info)
         {
             if (info.Count == 0)
             {
                 List<EquationComponent> components = new List<EquationComponent>(){
                     Nine, Add, Eight, Substract, Five, Equal, One, Two};
                 Equation guess = new Equation(components);
-                
+
                 return guess;
             }
             else
             {
-                throw new NotImplementedException();
+                List<EquationComponent> components = new List<EquationComponent>(){
+                Three, Add, Four, Add, Seven, Equal, One, Four};
+                Equation guess = new Equation(components);
+
+                return guess;
+            }
+        }
+
+        public static long CountValidGuesses(int digitCount)
+        {
+            // Enum count = 15
+            // Possible places = 8
+            // => 15^8 => base 15 system
+            var componentCount = Enum.GetNames(typeof(EquationComponent)).Length;
+            long allPossibilities = (long)Math.Pow(componentCount, digitCount);
+
+            long validEquationCount = 0;
+            Parallel.For(0, allPossibilities, (i ) => {
+                List<EquationComponent> components = GenerateComponents(i, digitCount);
+                
+                if(Equation.ValidateSyntax(components))
+                {
+                    Equation eq = new Equation(components);
+                    if(eq.Validate())
+                    {
+                        validEquationCount++;
+                    }
+                }
+            });
+
+            return validEquationCount;
+        }
+
+        public static List<EquationComponent> GenerateComponents(long equationNumber, int digitCount)
+        {
+            List<EquationComponent> components = new List<EquationComponent>();
+            GetNextDigit(equationNumber, digitCount, components);
+            return components;
+        }
+
+        private static void GetNextDigit(long equationNumber, int digitCount, List<EquationComponent> components)
+        {
+            // Algorithm:
+            // i % 15 = rest
+            // rest => Enum value
+            // i / 15 = i
+            // if(i > 0) => recursion
+            EquationComponent digit = (EquationComponent)(equationNumber % 15);
+            components.Add(digit);
+            if(digitCount > 1)
+            {
+                GetNextDigit(equationNumber / 15, digitCount-1, components);
             }
         }
     }
--- tests/Solver.Tests/SolverTests.cs
+++ tests/Solver.Tests/SolverTests.cs
@@ -13,35 +13,79 @@ namespace Solver.Tests
         }
 
         [Test]
+        public void Given_EquationNumberZero_When_GenerateEquation_Then_ReturnCorrectEquation()
+        {
+            List<EquationComponent> components = new List<EquationComponent>(){
+                Zero, Zero, Zero, Zero, Zero, Zero, Zero, Zero};
+
+            Solver.GenerateComponents(0, 8).Should().BeEquivalentTo(components);
+        }
+
+        [Test]
+        public void Given_EquationNumberOne_When_GenerateEquation_Then_ReturnCorrectEquation()
+        {
+            List<EquationComponent> components = new List<EquationComponent>(){
+                One, Zero, Zero, Zero, Zero, Zero, Zero, Zero};
+
+            Solver.GenerateComponents(1, 8).Should().BeEquivalentTo(components);
+        }
+
+        [Test]
+        public void Given_DigitCountThree_When_CountValidGuesses_ReturnTen()
+        {
+            Solver.CountValidGuesses(3).Should().Be(10);
+        }
+
+        [Test]
+        public void Given_DigitCountFour_When_CountValidGuesses_ReturnTen()
+        {
+            Solver.CountValidGuesses(4).Should().Be(31);
+        }
+
+        [Test]
+        public void Given_DigitCountFive_When_CountValidGuesses_ReturnTen()
+        {
+            Solver.CountValidGuesses(5).Should().Be(516);
+        }
+
+        [Test]
         public void Given_NoInformation_When_Solve_Then_Return_DefaultGuess()
         {
             List<EquationComponent> components = new List<EquationComponent>(){
                 Nine, Add, Eight, Substract, Five, Equal, One, Two};
 
             Equation expected = new Equation(components);
-
-            Dictionary<Equation, EquationComparison> info = new Dictionary<Equation, EquationComparison>();
-            Equation guess = Solver.Solve(info);
+            Equation guess = Solver.Guess(new List<EquationComparison>());
 
             guess.Should().BeEquivalentTo(expected);
         }
 
-        // [Test]
-        // [Ignore("No ready to be implemented")]
-        // public void Given_NoInformation_When_Solve_Then_Return_DefaultGuessAAAA()
-        // {
-        //     Equation expected = new Equation(
-        //         new List<long>() { 9, 8, 3, 14 },
-        //         new List<Operator>() { Operator.Add, Operator.Substract, Operator.Equal }
-        //     );
+        [Test]
+        public void Given_NoInformation_When_Solve_Then_Return_DefaultGuessA()
+        {
+            List<EquationComponent> components = new List<EquationComponent>(){
+                Nine, Add, Eight, Substract, Five, Equal, One, Two};
+            Equation guess = new Equation(components);
 
-        //     Dictionary<Equation, EquationComparison> info = new Dictionary<Equation, EquationComparison>();
-        //     Equation guess = Solver.Solve(info);
-        //     EquationComparison comparison = expected.Compare(guess);
-        //     info.Add(guess, comparison);
-        //     guess = Solver.Solve(info);
+            var comparison = new List<ComparisonStatus>() {
+                    ComparisonStatus.False,
+                    ComparisonStatus.Correct,
+                    ComparisonStatus.False,
+                    ComparisonStatus.False,
+                    ComparisonStatus.False,
+                    ComparisonStatus.Correct,
+                    ComparisonStatus.Correct,
+                    ComparisonStatus.False
+            };
+            EquationComparison eqComparison = new EquationComparison(guess, comparison);
 
-        //     guess.Should().BeEquivalentTo(expected);
-        // }
+            List<EquationComponent> componentsExpected = new List<EquationComponent>(){
+                Three, Add, Four, Add, Seven, Equal, One, Four};
+            Equation expected = new Equation(componentsExpected);
+
+            Equation result = Solver.Guess(new List<EquationComparison>() { eqComparison });
+
+            expected.Should().BeEquivalentTo(result);
+        }
     }
 }
